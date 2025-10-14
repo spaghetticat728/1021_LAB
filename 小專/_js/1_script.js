f224535f6a0e5f5d5e5da801e9d0a03cdab6d1d3
spaghetticat728 <monicaw123564@gmail.com>
2025-10-14 16:41:44 +0800

    1014_newJS

--- 小專/_js/1_script.js
+++ 小專/_js/1_script.js
@@ -1,231 +1,195 @@
-// --------
-// Login
-// --------
+// ------------
+// login
+// ------------
 
-function login(username) {
-    localStorage.setItem('user', JSON.stringify({ name: username, token: 'fake-' + Date.now() }));
+// 模擬帳號
+const USER1 = {
+    username: 'wrz',
+    password: '1234'
 }
 
+// 帳密正確 -> user存進localStorage裡面 -> 登入成功
+function login(username, password) {
+    if (username == USER1.username && password == USER1.password) {
+        localStorage.setItem('user', JSON.stringify({ name: username, token: 'fake-' + Date.now() }))
+        alert(username + "，歡迎!");
+        window.location.href = './小專主頁.html';
+    }
+    else {
+        alert("帳號或密碼錯誤");
+    }
+}
+
+// 檢驗是否登入(localStorage是否有值)
 // 回傳布林值
 function isLogin() {
     return !!localStorage.getItem('user');
 }
 
+// 從localStorage取得user資料並回傳陣列(物件?)
+// 若沒有 -> 回傳空字串
 function getUsername() {
     const u = localStorage.getItem('user');
     return u ? JSON.parse(u).name : '';
 }
 
+// 登出 -> 刪除user, cart, fav
 function logout() {
     localStorage.removeItem('user');
     localStorage.removeItem('cart');
-    localStorage.removeItem('favorites');
+    localStorage.removeItem('fav');
 }
 
-// 綁定登入按鈕事件
+// 綁定登入按鈕(登入頁)
+const btnLogin = document.getElementById("btnLogin");
 
-// 驗證用
-const btnLogin = document.getElementById("btnLogIn");
-// 綁定
 if (btnLogin) {
-    $('#btnLogIn').on('click', function () {
-        // 抓username
-        // trim -> 去除字串前後空白
-        const username = document.getElementById('username').value.trim();
+    btnLogin.addEventListener('click', function () {
+        // 抓input username中值，存進變數username
+        // trim -> 去除字串前後空白 -> 最後沒用
+        const username = document.getElementById('username').value;
+        const pass = document.getElementById('pass').value;
+        // 找到username -> 拿去login()存localStorage -> alert suc
         if (username) {
-            login(username);
-            alert('登入成功!');
-            window.location.href = '小專主頁.html';
+            login(username, pass);
         }
         else {
-            alert('請輸入帳號');
+            alert("請輸入帳號");
         }
     })
 }
 
 // 顯示登入狀態
-// function updateNavLogin() {
-//     const btnLogin = document.getElementById('btnLogin');
-//     if (isLogin()) {
-//         btnLogin.textContent = '登出';
-//         btnLogin.href = '#';
-//         btnLogin.onclick = () => {
-//             logout();            // 清除 localStorage
-//             updateNavLogin();    // 立即更新導覽列文字
-//             alert('已登出');
-//         };
-
-// document.getElementById('btnLogin').remove();
-// // loginArea.innerHTML += `<button id="btnLogout">登出</button>`;
-// const loginArea = document.getElementById('loginArea');
-// loginArea.appendChild(btnLogout);
-// const btnLogout = document.getElementById("btnLogout");
-// btnLogout.onclick = () => {
-//     logout();
-//     window.location.href = '小專主頁.html';
-// }   
-//     }
-
-//     else {
-//         btnLogin.textContent = '登入';
-//         btnLogin.href = '1_登入頁.html';
-//         btnLogin.onclick = null;
-//     }
-// }
-// updateNavLogin();
+// 大難題，先跳過
 
 
-function updateNavLogin() {
-    if (isLogin()) {
-        btnLogin.textContent = '登出';
-        btnLogin.href = '#';
-        btnLogin.onclick = (e) => {
-            // e.preventDefault();
-            logout();
-            updateNavLogin();
-            alert('已登出');
-        };
-    } else {
-        btnLogin.textContent = '登入';
-        btnLogin.href = '1_登入頁.html';
-        btnLogin.onclick = null;
-    }
-}
-
-updateNavLogin();
-// if(isLogin()){
-//     const btnLogin = document.getElementById('btnLogIn');
-//     if(btnLogin) btnLogin.remove();
-
-//     const loginArea = document.getElementById('loginArea');
-//     const btnLogout = document.createElement('button');
-//     btnLogout.id = 'btnLogout';
-//     btnLogout.textContent = '登出';
-//     btnLogout.onclick = () => {
-//         logout();
-//         window.location.href = '小專主頁.html';
-//     };
-//     loginArea.appendChild(btnLogout);
-// }
-
-
-// --------------
+//--------------
 // 購物車
-// --------------
+//--------------
+
+// 讀取localStorage中cart內容，並回傳(陣列)
+// 若沒有 -> 回傳空字串
 function loadCart() {
-    return JSON.parse(localStorage.getItem('cart') || '');
+    return JSON.parse(localStorage.getItem('cart')) || [];
 }
 
+// cart內容存進localStorage
 function saveCart(cart) {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
+// 商品(item)加入購物車
+// 驗證是否登入 -> 尋找該商品是否已在購物車 -> 是，qty+=1
+//                                       -> 否，push且新增qty屬性
 function addToCart(item) {
-    if (!islogin()) {
+    if (!isLogin()) {
         alert("請先登入!");
         return;
     }
     const cart = loadCart();
-    const found = cart.find(function (i) {
-        return i.id === item.id;
-    });
-
+    // 尋找該商品是否已在購物車
+    // 回傳item(key?)
+    // 若沒有 -> 回傳0
+    const found = cart.find(i => i.id == item.id);
     if (found) {
         found.qty += 1;
     }
-    else cart.push({ ...item, qty: 1 });
+    else {
+        cart.push({ ...item, qty: 1 });
+    }
+    // 新cart存回localStorage
     saveCart(cart);
     alert("已加入購物車");
-    // 箭頭函式寫法 -> cart.find(i => i.id === item.id)
 }
 
+// 綁定加入購物車事件
+document.getElementById('btnAddCart').addEventListener('click', addToCart);
+
 
 // ------------
-// 最愛
+// 我的最愛
 // ------------
+
+// 讀取localStorage中fav內容，並回傳(陣列)
+// 若沒有 -> 回傳空字串
 function loadFav() {
-    return JSON.parse(localStorage.getItem('fav'));
+    return JSON.parse(localStorage.getItem('fav')) || [];
 }
 
-function saveFav(favs) {
+// fav內容存回localStorage
+function saveFav(fav) {
     localStorage.setItem('fav', JSON.stringify(fav));
 }
 
+// 檢查item是否已在fav中
 // 回傳布林值
-function isFav(item) {
+function isFav(item){
     const fav = loadFav();
-    return fav.some(function (i) {
-        return i.id === item.id;
-    })
+    return fav.some(i => i.id == item.id);
 }
 
-function toggleFav(item) {
-    if (!isLogin) {
-        alert("請先登入!");
+// 商品(item)加入最愛
+// 驗證是否登入 -> 尋找該商品是否已在最愛 -> 是，移除
+//                                     -> 否，加入
+function addToFav(item){
+    if(!isLogin()){
+        alert("請先登入");
         return;
     }
 
-    let fav = loadFav(); // 陣列
-    const index = fav.findIndex(i => i.id === item.id); // true -> index, false -> -1
-    if (index === -1) {
+    const fav = loadFav();
+    // 尋找是否有相同商品，若有 -> 將編號存入index
+    // 若沒有 -> -1
+    const index =  fav.findIndex(i => i.id == item.id);
+    if(index == -1){
         fav.push(item);
         alert(item.name + '加入最愛');
     }
-    else {
+    else{
         fav.splice(index, 1);
         alert(item.name + '從最愛移除');
     }
     saveFav();
 }
 
+// 商品頁資料填入
+const productName = document.getElementById('productName');
+const productPrice = document.getElementById('productPrice');
 
-// 商品頁初始化
-// ---------------
-
-const btnAddCart = document.getElementById('btnAddCart');
-const btnFav = document.getElementById('btnFav');
-
-// 商品資料
+// 商品資料模板
 const product = {
     id: 'p001',
     name: 'Twista火棍',
     price: 1200
 }
 
-// 輸入商品頁資訊
-const productName = document.getElementById('productName');
-const productPrice = document.getElementById('productPrice');
-
-if (productName) {
+// productName, productPrice填入頁面顯示
+if (productName){
     productName.textContent = product.name;
 }
-if (productPrice) {
-    productPrice.textContent = '價格:' + product.priceㄤ
-}
-
-// 綁定購物車按鈕
-if (btnAddCart) {
-    btnAddCart.addEventListener('click', () => addToCart(product));
+if(productPrice){
+    productPrice.textContent = '$' + product.price;
 }
 
 // 最愛按鈕樣式切換
-if (btnFav) {
-    function updateFavBtn() {
-        if (isFavorited(product)) {
-            btnFav.classList.add('fav');
-            btnFav.textContent = '❤已收藏'
+// 檢查按鈕存在，才執行
+if(btnAddFav) {
+    function updateFavBtn(){
+        if (isFav(product)){
+            btnAddFav.classList.add('fav');
+            btnAddFav.textContent = '❤已收藏'
         }
         else {
-            btnFav.classList.remove('fav');
-            btnFav.textContent = '加入最愛';
+            btnAddFav.classList.remove('fav');
+            btnAddFav.textContent = '♡加入最愛';
         }
     }
 
-    updateFavBtn(); // 頁面一開始就執行，確保一開始就顯示正確的
+    // 頁面一開始就執行，確保一開始就顯示正確的
+    updateFavBtn();
 
-    // 綁定最愛按鈕事件
-    btnFav.addEventListener('click', () => {
-        toggleFav(product);
+    btnAddFav.addEventListener('click', () => {
+        addToFav(product);
         updateFavBtn();
     })
 }
---
+++ 小專/_js/1_script_error.js
@@ -0,0 +1,244 @@
+// --------
+// Login
+// --------
+// document.getElementById('btnFav').addEventListener('click', () => {
+//     window.location.href = '1_我的最愛.html';
+// })
+
+// document.getElementById('btnCart').addEventListener('click', () => {
+//     window.location.href = './1_購物車頁.html';
+// })
+
+// document.getElementById('btnlogin').addEventListener('click', () => {
+//     window.location.href = './1_登入頁.html';
+// })
+
+function login(username) {
+    localStorage.setItem('user', JSON.stringify({ name: username, token: 'fake-' + Date.now() }));
+}
+
+// 回傳布林值
+function isLogin() {
+    return !!localStorage.getItem('user');
+}
+
+function getUsername() {
+    const u = localStorage.getItem('user');
+    return u ? JSON.parse(u).name : '';
+}
+
+function logout() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('cart');
+    localStorage.removeItem('favorites');
+}
+
+// 綁定登入按鈕事件
+
+// 驗證用
+const btnLogin = document.getElementById("btnLogIn");
+// 綁定
+if (btnLogin) {
+    $('#btnLogIn').on('click', function () {
+        // 抓username
+        // trim -> 去除字串前後空白
+        const username = document.getElementById('username').value.trim();
+        if (username) {
+            login(username);
+            alert('登入成功!');
+            window.location.href = '小專主頁.html';
+        }
+        else {
+            alert('請輸入帳號');
+        }
+    })
+}
+
+// 顯示登入狀態
+// function updateNavLogin() {
+//     const btnlogin = document.getElementById('btnlogin');
+//     if (isLogin()) {
+//         btnlogin.textContent = '登出';
+//         btnlogin.href = '#';
+//         btnlogin.onclick = () => {
+//             logout();            // 清除 localStorage
+//             updateNavLogin();    // 立即更新導覽列文字
+//             alert('已登出');
+//         };
+//     }
+// }
+
+// document.getElementById('btnLogin').remove();
+// // loginArea.innerHTML += `<button id="btnLogout">登出</button>`;
+// const loginArea = document.getElementById('loginArea');
+// loginArea.appendChild(btnLogout);
+// const btnLogout = document.getElementById("btnLogout");
+// btnLogout.onclick = () => {
+//     logout();
+//     window.location.href = '小專主頁.html';
+// }   
+//     }
+
+//     else {
+//         btnLogin.textContent = '登入';
+//         btnLogin.href = '1_登入頁.html';
+//         btnLogin.onclick = null;
+//     }
+// }
+// updateNavLogin();
+
+
+// function updateNavLogin() {
+//     if (isLogin()) {
+//         btnLogin.textContent = '登出';
+//         btnLogin.href = '#';
+//         btnLogin.onclick = (e) => {
+//             // e.preventDefault();
+//             logout();
+//             updateNavLogin();
+//             alert('已登出');
+//         };
+//     } else {
+//         btnlogin.textContent = '登入';
+//         btnlogin.href = '1_登入頁.html';
+//         btnlogin.onclick = null;
+//     }
+// }
+
+// updateNavLogin();
+// if(isLogin()){
+//     const btnLogin = document.getElementById('btnLogIn');
+//     if(btnLogin) btnLogin.remove();
+
+//     const loginArea = document.getElementById('loginArea');
+//     const btnLogout = document.createElement('button');
+//     btnLogout.id = 'btnLogout';
+//     btnLogout.textContent = '登出';
+//     btnLogout.onclick = () => {
+//         logout();
+//         window.location.href = '小專主頁.html';
+//     };
+//     loginArea.appendChild(btnLogout);
+// }
+
+
+// --------------
+// 購物車
+// --------------
+function loadCart() {
+    return JSON.parse(localStorage.getItem('cart') || '');
+}
+
+function saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function addToCart(item) {
+    if (!islogin()) {
+        alert("請先登入!");
+        return;
+    }
+    const cart = loadCart();
+    const found = cart.find(function (i) {
+        return i.id === item.id;
+    });
+
+    if (found) {
+        found.qty += 1;
+    }
+    else cart.push({ ...item, qty: 1 });
+    saveCart(cart);
+    alert("已加入購物車");
+    // 箭頭函式寫法 -> cart.find(i => i.id === item.id)
+}
+
+
+// ------------
+// 最愛
+// ------------
+function loadFav() {
+    return JSON.parse(localStorage.getItem('fav') || '[]');
+}
+
+function saveFav(favs) {
+    localStorage.setItem('fav', JSON.stringify(fav));
+}
+
+// 回傳布林值
+function isFav(item) {
+    const fav = loadFav();
+    return fav.some(function (i) {
+        return i.id === item.id;
+    })
+}
+
+function toggleFav(item) {
+    if (!isLogin) {
+        alert("請先登入!");
+        return;
+    }
+
+    let fav = loadFav(); // 陣列
+    const index = fav.findIndex(i => i.id === item.id); // true -> index, false -> -1
+    if (index === -1) {
+        fav.push(item);
+        alert(item.name + '加入最愛');
+    }
+    else {
+        fav.splice(index, 1);
+        alert(item.name + '從最愛移除');
+    }
+    saveFav();
+}
+
+
+// 商品頁初始化
+// ---------------
+
+const btnAddCart = document.getElementById('btnAddCart');
+const btnAddFav = document.getElementById('btnAddFav');
+
+// 商品資料
+const product = {
+    id: 'p001',
+    name: 'Twista火棍',
+    price: 1200
+}
+
+// 輸入商品頁資訊
+const productName = document.getElementById('productName');
+const productPrice = document.getElementById('productPrice');
+
+if (productName) {
+    productName.textContent = product.name;
+}
+if (productPrice) {
+    productPrice.textContent = '價格:' + product.price;
+}
+
+// 綁定購物車按鈕
+if (btnAddCart) {
+    btnAddCart.addEventListener('click', () => addToCart(product));
+}
+
+// 最愛按鈕樣式切換
+if (btnFav) {
+    function updateFavBtn() {
+        if (isFav(product)) {
+            btnFav.classList.add('fav');
+            btnFav.textContent = '❤已收藏'
+        }
+        else {
+            btnFav.classList.remove('fav');
+            btnFav.textContent = '加入最愛';
+        }
+    }
+
+    updateFavBtn(); // 頁面一開始就執行，確保一開始就顯示正確的
+
+    // 綁定最愛按鈕事件
+    btnFav.addEventListener('click', () => {
+        toggleFav(product);
+        updateFavBtn();
+    })
+}
