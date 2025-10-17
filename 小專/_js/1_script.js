beb24884b03b9a10df780bc62a0e8d4b2d66a520
spaghetticat728 <monicaw123564@gmail.com>
2025-10-17 15:28:39 +0800

    1017_暫定完成_第一版

--- 小專/_js/1_script.js
+++ 小專/_js/1_script.js
@@ -62,6 +62,23 @@ if (btnLogin) {
 // 顯示登入狀態
 // 大難題，先跳過
 
+// login 主頁
+// Login 登入頁
+
+function updateNavLogin() {
+    const btnlogin = document.getElementById('btnlogin');
+    if (isLogin()) {
+        btnlogin.textContent = "登出";
+        btnlogin.href = "./小專主頁.html";
+        btnlogin.addEventListener('click', () => {
+            logout();
+            updateNavLogin();
+            alert('已登出');
+        });
+    }
+}
+updateNavLogin();
+
 
 //--------------
 // 購物車
@@ -99,7 +116,7 @@ function addToCart(item) {
         found.qty += qtyInput;
     }
     else {
-        cart.push({ ...item, option: option, qty: qtyInput});
+        cart.push({ ...item, option: option, qty: qtyInput });
     }
     // 新cart存回localStorage
     saveCart(cart);
@@ -107,8 +124,11 @@ function addToCart(item) {
 }
 
 // 綁定加入購物車事件
-document.getElementById('btnAddCart').addEventListener('click', () => addToCart(product));
-
+const btnAddCart = document.getElementById('btnAddCart');
+if (btnAddCart) {
+    btnAddCart.addEventListener('click', () => addToCart(product));
+    // console.log("111");
+}
 
 // ------------
 // 我的最愛
@@ -127,7 +147,7 @@ function saveFav(fav) {
 
 // 檢查item是否已在fav中
 // 回傳布林值
-function isFav(item){
+function isFav(item) {
     const fav = loadFav();
     return fav.some(i => i.id == item.id);
 }
@@ -135,8 +155,8 @@ function isFav(item){
 // 商品(item)加入最愛
 // 驗證是否登入 -> 尋找該商品是否已在最愛 -> 是，移除
 //                                     -> 否，加入
-function addToFav(item){
-    if(!isLogin()){
+function addToFav(item) {
+    if (!isLogin()) {
         alert("請先登入");
         return;
     }
@@ -144,12 +164,12 @@ function addToFav(item){
     const fav = loadFav();
     // 尋找是否有相同商品，若有 -> 將編號存入index
     // 若沒有 -> -1
-    const index =  fav.findIndex(i => i.id == item.id);
-    if(index == -1){
+    const index = fav.findIndex(i => i.id == item.id);
+    if (index == -1) {
         fav.push(item);
         alert(item.name + '加入最愛');
     }
-    else{
+    else {
         fav.splice(index, 1);
         alert(item.name + '從最愛移除');
     }
@@ -159,8 +179,9 @@ function addToFav(item){
 // 商品頁資料填入
 const productName = document.getElementById('productName');
 const productPrice = document.getElementById('productPrice');
+// const image = document.getElementById('image');
 
-// 商品資料模板
+// 商品資料
 // const product = {
 //     id: 'p001',
 //     name: 'Twista火棍',
@@ -168,18 +189,23 @@ const productPrice = document.getElementById('productPrice');
 // }
 
 // productName, productPrice填入頁面顯示
-if (productName){
+if (productName) {
     productName.textContent = product.name;
 }
-if(productPrice){
+if (productPrice) {
     productPrice.textContent = '$' + product.price;
 }
 
+// 更新收藏紐狀態]
+
+
 // 最愛按鈕樣式切換
 // 檢查按鈕存在，才執行
-if(btnAddFav) {
-    function updateFavBtn(){
-        if (isFav(product)){
+const btnAddFav = document.getElementById('btnAddFav');
+if (btnAddFav) {
+
+    function updateFavBtn() {
+        if (isFav(product)) {
             btnAddFav.classList.add('fav');
             btnAddFav.textContent = '❤已收藏'
         }
@@ -188,12 +214,42 @@ if(btnAddFav) {
             btnAddFav.textContent = '♡加入最愛';
         }
     }
-
     // 頁面一開始就執行，確保一開始就顯示正確的
     updateFavBtn();
 
     btnAddFav.addEventListener('click', () => {
         addToFav(product);
         updateFavBtn();
-    })
+    });
 }
+
+
+
+
+document.querySelectorAll('.card').forEach(card => {
+    const product = {
+        id: card.dataset.id,
+        name: card.dataset.name,
+        price: card.dataset.price,
+        img: card.dataset.img
+    }
+
+    const btnAddFav = card.querySelector('.btnAddFav');
+    if (!btnAddFav) return;
+    function updateFavBtn() {
+        if (isFav(product)) {
+            btnAddFav.classList.add('fav');
+            btnAddFav.textContent = '❤已收藏'
+        }
+        else {
+            btnAddFav.classList.remove('fav');
+            btnAddFav.textContent = '♡加入最愛';
+        }
+    }
+    updateFavBtn();
+
+    btnAddFav.addEventListener('click', () => {
+        addToFav(product);
+        updateFavBtn();
+    })
+})
